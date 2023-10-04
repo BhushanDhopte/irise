1019cb6e5310aba9f7d47135dcb4d293cf3a2de6
BhushanDhopte <144302290+BhushanDhopte@users.noreply.github.com>
2023-10-04 09:52:34 +0530

    Day19 update

---
+++ Javasript/day-1-js.js
@@ -0,0 +1,21 @@
+console.log("Hello!")
+var Name="Bhushan Dhopte"
+var Mobile=9834475806
+var city='khamgaon'
+console.log("My name is "+Name)
+console.log("My Mobile number is "+Mobile )
+console.log("I am from "+city)
+console.log()
+console.log(`Today we do some arthmatic operations`)
+console.log()
+var x=100
+var y=20
+var A=x+y
+var B=x-y
+var C=x*y
+var D=x/y
+console.log("The Addition of two numbers="+A)
+console.log("The Subtraction of two numbers="+B)
+console.log("The Multification of two numbers="+C)
+console.log("The Division of two numbers="+D)
+console.log()
