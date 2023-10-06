7461c7899a3c6a41a66369cd1dc20c3abb55b9da
BhushanDhopte <144302290+BhushanDhopte@users.noreply.github.com>
2023-10-06 09:42:48 +0530

    update

---
+++ Javasript/day-3-js.js
@@ -0,0 +1,20 @@
+var X=24
+
+var Y=30
+var Z=28
+
+if(X%2==0){
+
+    if(Y%2==0){
+        console.log(`${Y} is a Even number`)
+    }else{
+        console.log(`${Y} is a Old number`)
+    }
+}else{
+
+    if(Z%2==0){
+        console.log(`${Z} is a Even number`)
+    }else{
+        console.log(`${Z} is a Old number`)
+    }
+}
