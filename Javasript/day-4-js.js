398018e1247dfc3f0a30cc92da8d809c533289ab
BhushanDhopte <144302290+BhushanDhopte@users.noreply.github.com>
2023-10-09 09:42:00 +0530

    update

---
+++ Javasript/day-4-js.js
@@ -0,0 +1,17 @@
+// var x;
+// for(x=2;x<=20;x++){
+
+//     console.log(x);
+//     x++
+
+// }
+
+function cal(x,y){
+
+    console.log("the addition of two number is ",x+y)
+    console.log("the subtraction of two number is ",x-y)
+    console.log("the multification of two number is ",x*y)
+    console.log("the division of two number is ",x/y)
+}
+
+cal(100,25)
